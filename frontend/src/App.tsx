85e81f08646783fd6197e5c5dc08b60b72728a3d
SamadeshPoudel <poudelsamadesh@gmail.com>
2026-01-16 12:49:10 +0530

    vite init with tailwind setup for fe

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return(
+    <div>
+      {/* navbar  */}
+      <div className="border border-b-2 h-8">
+        <p className="w-full bg-yellow-300 text-center">whats up</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
