c82dcbef17cb2537d723504fc9e6caea9dc95edf
SamadeshPoudel <poudelsamadesh@gmail.com>
2026-01-29 16:09:41 +0530

    added ws reconnection logic to backpack in the poller server

--- poller/index.ts
+++ poller/index.ts
@@ -1,12 +1,6 @@
 import WebSocket, { WebSocketServer } from "ws"
 import { createClient } from "redis"
-
-//listen to backpack for the latest price
-const ws = new WebSocket(process.env.BACKPACK_API!)
 const client = createClient();
-
-//creating wss to send ws data to frontend from this poller ws
-const wss = new WebSocketServer({ port: 8080 })
 
 interface Payload {
     action: string,
@@ -16,55 +10,80 @@ interface Payload {
     decimal: number
 }
 
-ws.on('error', console.error)
-
-ws.on('open', async () => {
-    console.log("ws connected")
+async function main() {
+    //only connect to redis once on startup
     await client.connect();
     console.log("redis client connected")
-    ws.send('{ "method": "SUBSCRIBE", "params": ["bookTicker.SOL_USDC", "bookTicker.BTC_USDC", "bookTicker.ETH_USDC"] }')
-})
 
-ws.on('message', async (event) => {
-    const stream = JSON.parse(event.toString())
-    const AskSpread = 0.999;
-    const BidSpread = 1.001;
-    const askWithSpread = parseFloat(stream.data.a) * AskSpread
-    const bidWithSpread = parseFloat(stream.data.b) * BidSpread
-    // console.log("Type of askWithSpread",typeof askWithSpread)
-    // console.log("Type of bidWithSpread",typeof bidWithSpread)
+    //start backpack ws connection
+    connectToBackpack();
+}
+
+function connectToBackpack() {
+
+    //listen to backpack for the latest price
+    const ws = new WebSocket(process.env.BACKPACK_API!)
+
+    ws.on('error', console.error)
+
+    ws.on('open', async () => {
+        console.log("ws connected")
+        ws.send('{ "method": "SUBSCRIBE", "params": ["bookTicker.SOL_USDC", "bookTicker.BTC_USDC", "bookTicker.ETH_USDC"] }')
+    })
+
+    ws.on('message', async (event) => {
+        const stream = JSON.parse(event.toString())
+        const AskSpread = 0.999;
+        const BidSpread = 1.001;
+        const askWithSpread = parseFloat(stream.data.a) * AskSpread
+        const bidWithSpread = parseFloat(stream.data.b) * BidSpread
+        // console.log("Type of askWithSpread",typeof askWithSpread)
+        // console.log("Type of bidWithSpread",typeof bidWithSpread)
 
 
-    const payload: Payload = {
-        action: "PRICE_UPDATE",
-        asset: (stream.data.s).split("_")[0], //SOL_USDC
-        askWithSpread: Math.trunc(askWithSpread * 10000),
-        bidWithSpread: Math.trunc(bidWithSpread * 10000),
-        decimal: 4
+        const payload: Payload = {
+            action: "PRICE_UPDATE",
+            asset: (stream.data.s).split("_")[0], //SOL_USDC
+            askWithSpread: Math.trunc(askWithSpread * 10000),
+            bidWithSpread: Math.trunc(bidWithSpread * 10000),
+            decimal: 4
 
-    }
-
-    await client.XADD(
-        "trade", //stream name
-        "*", //auto gen id with timestamp
-        {
-            data: JSON.stringify(payload)
         }
-    )
 
-    if (!subscriptionTable.has(payload.asset)) {
-        subscriptionTable.set(payload.asset, [])
-        return;
-    }
-    subscriptionTable.get(payload.asset)?.forEach(ws => ws.send(JSON.stringify(payload)))
+        await client.XADD(
+            "trade", //stream name
+            "*", //auto gen id with timestamp
+            {
+                data: JSON.stringify(payload)
+            }
+        )
 
-    // console.log("Asset:", payload.asset);
-    // console.log("Ask with spread",payload.askWithSpread);
-    // console.log("Bid with spread",payload.bidWithSpread);
+        if (!subscriptionTable.has(payload.asset)) {
+            subscriptionTable.set(payload.asset, [])
+            return;
+        }
+        subscriptionTable.get(payload.asset)?.forEach(ws => ws.send(JSON.stringify(payload)))
 
-    // console.log("stream pushed to redis stream")
-    // console.log("\n")
-})
+        // console.log("Asset:", payload.asset);
+        // console.log("Ask with spread",payload.askWithSpread);
+        // console.log("Bid with spread",payload.bidWithSpread);
+
+        // console.log("stream pushed to redis stream")
+        // console.log("\n")
+    })
+
+    ws.on('close', () => {
+        console.log("Backpack ws closed, reconnecting in 3 seconfs");
+        setTimeout(() => {
+            connectToBackpack();
+        }, 3000)
+    })
+}
+
+main();
+
+//creating wss to send ws data to frontend from this poller ws
+const wss = new WebSocketServer({ port: 8080 })
 
 const subscriptionTable = new Map<string, WebSocket[]>() //key is SOL_USDC 
 
